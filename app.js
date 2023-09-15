5affacbfdf865ea79b9e0d79b9fff80061f885e9
kurodoke <ariefsatrio098@gmail.com>
2023-09-15 16:17:38 +0700

    fix

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cors());
 
 app.use((req, res, next) => {
     const token = req.headers.authorization;
-
+    next();
     // if (token && isValidToken(token)) {
     //     next();
     // } else {
